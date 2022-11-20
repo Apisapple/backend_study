f4c811169755de8c93d9005e8d530cf5eeabad5b
Apisapple <38449976+Apisapple@users.noreply.github.com>
2022-11-20 16:16:26 +0900

    Init project
    init demo shopping backend server

---
+++ demo_shopping/src/test/java/com/example/demo_shopping/DemoShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoShoppingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
