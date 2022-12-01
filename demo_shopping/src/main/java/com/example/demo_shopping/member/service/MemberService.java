0be5bbf9e85c8fcda4ae1d2f141b72134658d596
Apisapple <38449976+Apisapple@users.noreply.github.com>
2022-12-02 01:26:52 +0900

    Add member service
    멤버 서비스 기초 추가

---
+++ demo_shopping/src/main/java/com/example/demo_shopping/member/service/MemberService.java
@@ -0,0 +1,9 @@
+package com.example.demo_shopping.member.service;
+
+public interface MemberService {
+  void joinMember();
+
+  void updateMember();
+
+  void removeMember();
+}
---
+++ demo_shopping/src/main/java/com/example/demo_shopping/member/service/MemberServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.demo_shopping.member.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+  @Override
+  public void joinMember() {
+
+  }
+
+  @Override
+  public void updateMember() {
+
+  }
+
+  @Override
+  public void removeMember() {
+
+  }
+}
