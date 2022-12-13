78c540847a8e706093f7867e4251b989ebd911d4
Apisapple <38449976+Apisapple@users.noreply.github.com>
2022-12-14 02:25:11 +0900

    Add Swagger 3.0
    Swagger3.0 적용

--- demo_shopping/src/main/java/com/example/demo_shopping/member/controller/MemberController.java
+++ demo_shopping/src/main/java/com/example/demo_shopping/member/controller/MemberController.java
@@ -5,9 +5,15 @@ import com.example.demo_shopping.member.data.MemberJoinResponseData;
 import com.example.demo_shopping.member.exception.AddressErrorException;
 import com.example.demo_shopping.member.exception.MemberErrorException;
 import com.example.demo_shopping.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -18,7 +24,7 @@ public class MemberController {
   private final MemberService memberService;
 
   @PostMapping("/member/join")
-  public ResponseEntity<MemberJoinResponseData> joinMember(MemberDto memberDto) {
+  public ResponseEntity<MemberJoinResponseData> joinMember(@RequestBody MemberDto memberDto) {
     try {
       return ResponseEntity.ok().body(memberService.joinMember(memberDto));
     } catch (AddressErrorException | MemberErrorException e) {
