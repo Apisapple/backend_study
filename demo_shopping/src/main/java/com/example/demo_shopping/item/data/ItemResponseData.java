b7bcf7aa59d243dd716fb2fcd1fdae5f132a8b53
Apisapple <kimminyong2034@gmail.com>
2023-01-07 09:27:46 +0900

    Modify ItemService

---
+++ demo_shopping/src/main/java/com/example/demo_shopping/item/data/ItemResponseData.java
@@ -0,0 +1,10 @@
+package com.example.demo_shopping.item.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemResponseData {
+  private Long id;
+}
--- demo_shopping/src/main/java/com/example/demo_shopping/item/service/ItemService.java
+++ demo_shopping/src/main/java/com/example/demo_shopping/item/service/ItemService.java
@@ -1,11 +1,7 @@
 package com.example.demo_shopping.item.service;
 
-import com.example.demo_shopping.item.repository.ItemRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import com.example.demo_shopping.item.data.ItemResponseData;
 
-@Service
-@RequiredArgsConstructor
-public class ItemService {
-  private final ItemRepository itemRepository;
+public interface ItemService {
+  ItemResponseData findItemById(Long id);
 }
