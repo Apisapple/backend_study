c1926961db2a6c79a235519d97da06434edfef15
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-04-02 23:53:05 +0900

    [FEAT] Update MemberService
    Update MemberService

--- web_novel/src/main/java/com/example/web_novel/user/service/MemberService.java
+++ web_novel/src/main/java/com/example/web_novel/user/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.example.web_novel.user.service;
 
+import com.example.web_novel.user.entity.Member;
 import com.example.web_novel.user.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,5 +11,48 @@ public class MemberService {
 
   private final MemberRepository memberRepository;
 
-   
+  /**
+   * Saved member
+   *
+   * @param member member information
+   * @return saved member
+   */
+  public Member saveMember(Member member) {
+    return memberRepository.save(member);
+  }
+
+
+  /**
+   * buy point function
+   *
+   * @param name member name
+   * @param point mount of point
+   * @return member information
+   */
+  public Member buyPoint(String name, Integer point) {
+    Member savecMember = memberRepository.findByName(name).orElseThrow(
+        () -> new IllegalArgumentException("CANNOT FOUNT MEMBER")
+    );
+
+    savecMember.addPoint(point);
+
+    return savecMember;
+  }
+
+  /**
+   * use point function
+   *
+   * @param name member name
+   * @param point mount of point
+   * @return member information
+   */
+  public Member usePoint(String name, Integer point) {
+    Member savecMember = memberRepository.findByName(name).orElseThrow(
+        () -> new IllegalArgumentException("CANNOT FOUNT MEMBER")
+    );
+
+    savecMember.minusPoint(point);
+
+    return savecMember;
+  }
 }
