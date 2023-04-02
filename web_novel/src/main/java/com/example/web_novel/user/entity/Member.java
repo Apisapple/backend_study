5f1ad2ef582525d86d1194c9bee2832a8c6a9e56
Apisapple <kimminyong2034@gmail.com>
2023-04-02 16:22:04 +0900

    [FEAT] Create Member Entity
    Create Member Entity

---
+++ web_novel/src/main/java/com/example/web_novel/user/entity/Member.java
@@ -0,0 +1,21 @@
+package com.example.web_novel.user.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Table(name = "member")
+@Entity
+public class Member {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private Integer point;
+}
