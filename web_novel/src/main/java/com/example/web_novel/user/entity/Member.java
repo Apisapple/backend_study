db6abd6d02de35a4e48d4a43b9452260495f7a02
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-04-02 23:40:04 +0900

    [FIX] Update Member class
    Add Member no argument constructor

--- web_novel/src/main/java/com/example/web_novel/user/entity/Member.java
+++ web_novel/src/main/java/com/example/web_novel/user/entity/Member.java
@@ -8,10 +8,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Table(name = "member")
 @Entity
+@NoArgsConstructor
 public class Member {
 
   @Id
